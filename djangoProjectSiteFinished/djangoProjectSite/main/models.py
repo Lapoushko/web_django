218ae6e76ba5753acad668a9cfb44db04b62cf7d
Lapoushko <vadim.dovgiy@mail.ru>
2023-01-25 12:14:56 +0500

    admin_Update

--- djangoProjectSiteFinished/djangoProjectSite/main/models.py
+++ djangoProjectSiteFinished/djangoProjectSite/main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Description(models.Model):
+    title = models.CharField('Название',max_length=50)
+    demand = models.TextField('Востребованность')
+    geography = models.TextField('География')
+    skills = models.TextField('Навыки')
+    lastvac = models.TextField('Последние вакансии')
+
+    def __str__(self):
+        return self.title
